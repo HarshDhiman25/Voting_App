6d854a2e6389f3f82fcc834d93b67ddfe6518326
Harshqdeveloper <Sunny959a@gmail.com>
2024-08-06 15:36:18 +0530

    CreateUser

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Voting_Test.Models;
+
+namespace Voting_Test.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ApplicationUser>ApplicationUsers { get; set; }
+    }
+}
