712ccdb774529c709b4d41a184dad3562e67d51c
Harshqdeveloper <Sunny959a@gmail.com>
2024-08-09 18:15:57 +0530

    it

--- Program.cs
+++ Program.cs
@@ -1,58 +1,4 @@
-//    using Microsoft.AspNetCore.Identity;
-//using Microsoft.AspNetCore.Identity.UI.Services;
-//using Microsoft.EntityFrameworkCore;
-//using Voting_Test;
-//using Voting_Test.Data;
-//using Voting_Test.Hubs;
 
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-//builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//    options.UseSqlServer(connectionString));
-
-
-
-//builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
-//{
-//    // Configure Identity options here.
-//    options.SignIn.RequireConfirmedAccount = true;
-//})
-//    .AddEntityFrameworkStores<ApplicationDbContext>()
-//    .AddDefaultTokenProviders();
-//builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
-//builder.Services.AddRazorPages();
-//builder.Services.AddScoped<IEmailSender, EmailSender>();    
-//builder.Services.AddSignalR();
-//builder.Services.AddSession();
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseMigrationsEndPoint();
-//}
-//else
-//{
-//    app.UseExceptionHandler("/Home/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
-
-//app.UseHttpsRedirection();
-//app.UseStaticFiles();
-
-//app.UseRouting();
-
-//app.UseAuthorization();
-
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller=Home}/{action=Index}/{id?}");
-//app.MapRazorPages();
-//app.MapHub<VoteHub>("/chatHub");
-//app.Run();
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
@@ -78,12 +24,12 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IEmailSender, EmailSender>();
-builder.Services.AddSignalR(); // Ensure SignalR is added to the service collection
+builder.Services.AddSignalR(); 
 builder.Services.AddSession();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -99,13 +45,13 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthentication(); // Add Authentication middleware
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
-app.MapHub<VoteHub>("/voteHub"); // Ensure the Hub endpoint is correctly mapped
+app.MapHub<VoteHub>("/voteHub"); 
 app.Run();
 
